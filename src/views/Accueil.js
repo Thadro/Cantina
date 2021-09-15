5b21525a84dbcee255c761372d9c55f29426f03e
Romain D <romain.deneuve@yahoo.fr>
2021-09-15 16:49:01 +0200

    Filter added -> Accueil

--- src/views/Accueil.js
+++ src/views/Accueil.js
@@ -1,10 +1,28 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-import { Card, Avatar } from "antd";
+import { Card, Radio, Select, Input, Form } from "antd";
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from "@ant-design/icons";
 
 const { Meta } = Card;
+
+const { Option } = Select;
+
+function onChange(value) {
+    console.log(`selected ${value}`);
+}
+
+function onBlur() {
+    console.log("blur");
+}
+
+function onFocus() {
+    console.log("focus");
+}
+
+function onSearch(val) {
+    console.log("search:", val);
+}
 
 const Accueil = () => {
     const [recettes, setRecettes] = useState(null);
@@ -22,9 +40,35 @@ const Accueil = () => {
         <>
             <div className="App">
                 <h1 className="titre-accueil">Liste des recettes</h1>
-                <form class="search-container">
-                    <input type="text" className="search-bar" />
-                </form>
+                <div className="filter-ctnr">
+                    <Form.Item name="rechercher" className="search-bar">
+                        <Input placeholder="Rechercher" />
+                    </Form.Item>
+                    <Radio.Group defaultValue="Padawan" style={{ marginTop: 16 }}>
+                        <Radio.Button value="Padawan">Padawan</Radio.Button>
+                        <Radio.Button value="Jedi">Jedi</Radio.Button>
+                        <Radio.Button value="Maitre Jedi">Maitre Jedi</Radio.Button>
+                    </Radio.Group>
+                    <Select
+                        showSearch
+                        style={{ width: 200 }}
+                        placeholder="Select a person"
+                        optionFilterProp="children"
+                        onChange={onChange}
+                        onFocus={onFocus}
+                        onBlur={onBlur}
+                        onSearch={onSearch}
+                        filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
+                        <Option value="1">1</Option>
+                        <Option value="2">2</Option>
+                        <Option value="3">3</Option>
+                        <Option value="4">4</Option>
+                        <Option value="5">5</Option>
+                        <Option value="6">6</Option>
+                        <Option value="7">7</Option>
+                        <Option value="8">8</Option>
+                    </Select>
+                </div>
                 <div className="recette-ctnr">
                     {recettes &&
                         recettes.map(recette => (
@@ -42,3 +86,20 @@ const Accueil = () => {
 };
 
 export default Accueil;
+
+{
+    /* <div className="radio-ctnr">
+                        <div className="radio-btn">
+                            <input type="radio" name="drone" value="Padawan" className="input-radio" checked />
+                            <label for="Padawan">Padawan</label>
+                        </div>
+                        <div className="radio-btn">
+                            <input type="radio" name="drone" value="Jedi" className="input-radio" checked />
+                            <label for="Jedi">Jedi</label>
+                        </div>
+                        <div className="radio-btn">
+                            <input type="radio" name="drone" value="Maître Jedi" className="input-radio" checked />
+                            <label for="Mâitre Jedi">Maître Jedi</label>
+                        </div>
+                    </div> */
+}
