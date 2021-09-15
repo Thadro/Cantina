db4487e06daa26d209dcfae295c00fdddf9fd2cf
Romain D <romain.deneuve@yahoo.fr>
2021-09-15 11:47:58 +0200

    CSS added -> Accueil

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
 import "./App.css";
 
 import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import "antd/dist/antd.css";
 
 import Accueil from "./views/Accueil";
 import Ajouter from "./views/Ajouter";
 import Infos from "./views/Infos";
 import Modification from "./views/Modification";
 
-//  http:/localhost:9000/api/recipes
+//  http://localhost:9000/api/recipes
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <header>
-          <NavLink to="/" activeClassName="selected" className="header-a" exact>
-            Accueil
-          </NavLink>
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <header>
+                    <NavLink to="/" activeClassName="selected" className="header-a" exact>
+                        Accueil
+                    </NavLink>
 
-          <NavLink
-            to="/ajouter"
-            activeClassName="selected"
-            className="header-a"
-            exact
-          >
-            Ajouter
-          </NavLink>
-        </header>
+                    <NavLink to="/ajouter" activeClassName="selected" className="header-a" exact>
+                        Ajouter
+                    </NavLink>
+                </header>
 
-        {/*Accueil */}
-        <Route path="/" component={Accueil} exact />
-        {/*Ajouter */}
-        <Route path="/ajouter" component={Ajouter} exact />
-      </div>
-    </BrowserRouter>
-  );
+                {/*Accueil */}
+                <Route path="/" component={Accueil} exact />
+                {/*Ajouter */}
+                <Route path="/ajouter" component={Ajouter} exact />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/views/Accueil.js
+++ src/views/Accueil.js
@@ -1,30 +1,39 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
+import { Card, Avatar } from "antd";
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from "@ant-design/icons";
+
+const { Meta } = Card;
+
 const Accueil = () => {
-  const [recettes, setRecettes] = useState(null);
+    const [recettes, setRecettes] = useState(null);
 
-  useEffect(() => {
-    fetch("http://localhost:9000/api/recipes")
-      .then((res) => res.json())
-      .then((recipes) => {
-        setRecettes(recipes);
-        console.log(recipes);
-      });
-  }, []);
+    useEffect(() => {
+        fetch("http://localhost:9000/api/recipes")
+            .then(res => res.json())
+            .then(recipes => {
+                setRecettes(recipes);
+                console.log(recipes);
+            });
+    }, []);
 
-  return (
-    <div className="App">
-      <h1>Liste des recettes</h1>
-      {recettes &&
-        recettes.map((recette) => (
-          <div key={recette.id}>
-            <h2>{recette.titre}</h2>
-            <p>{recette.description}</p>
-          </div>
-        ))}
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="titre-accueil">Liste des recettes</h1>
+            <div className="recette-ctnr">
+                {recettes &&
+                    recettes.map(recette => (
+                        <Card
+                            style={{ width: 300, margin: 25 }}
+                            cover={<img alt="example" src={recette.photo} />}
+                            actions={[<SettingOutlined key="setting" />, <EditOutlined key="edit" />, <EllipsisOutlined key="ellipsis" />]}>
+                            <Meta title={recette.titre} description={recette.description} />
+                        </Card>
+                    ))}
+            </div>
+        </div>
+    );
 };
 
 export default Accueil;
