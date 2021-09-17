719bd70d09fa0b3a8b7d0862d187deefa3192fc4
Romain D <romain.deneuve@yahoo.fr>
2021-09-17 11:30:30 +0200

    Infos recette ended

--- src/views/Infos.js
+++ src/views/Infos.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router";
 import { useState, useEffect } from "react";
-import { FieldTimeOutlined } from "@ant-design/icons";
+import { StarOutlined, TeamOutlined, FieldTimeOutlined } from "@ant-design/icons";
 
 function Infos() {
     let { id } = useParams();
@@ -18,19 +18,49 @@ function Infos() {
     }, []);
 
     return (
-        <div className="infos-global-ctnr">
-            {recette && (
-                <div className="img-des-ctnr">
-                    <img alt="image recette" src={recette.photo}></img>
-                    <div className="infos-icon-ctnr">
-                        <div className="infos-icon">
+        <>
+            <div className="infos-global-ctnr">
+                {recette && (
+                    <div className="img-des-ctnr">
+                        <h2>{recette.titre}</h2>
+                        <h3>{recette.description}</h3>
+                        <img alt="recette image" src={recette.photo}></img>
+                        <h4>{recette.etapes}</h4>
+                    </div>
+                )}
+                {recette && (
+                    <div className="infos-recette-ctnr">
+                        <div className="infos-recette">
+                            <StarOutlined />
+                            <p>{recette.niveau}</p>
+                        </div>
+
+                        <div className="infos-recette">
+                            <TeamOutlined />
+                            <p>{recette.personnes > 1 ? " personnes" : " personne"}</p>
+                        </div>
+                        <div className="infos-recette">
                             <FieldTimeOutlined />
                             <p>{recette.tempsPreparation} min</p>
                         </div>
+
+                        <div className="ingredients-recette">
+                            <h3>Liste des ingredients : </h3>
+                            {recette &&
+                                recette.ingredients.map(ingredient => (
+                                    <p>
+                                        {`${ingredient[0]} `}
+                                        {ingredient[1]}
+                                    </p>
+                                ))}
+                        </div>
+                        <div className="button-ctnr">
+                            <input className="modifier-btn" type="button" value="Modifier" />
+                        </div>
                     </div>
-                </div>
-            )}
-        </div>
+                )}
+            </div>
+        </>
     );
 }
 
